59a9b6f80ba9cd6c2099c801a264f1bdef5d62a5
Andrew McKnight <andrew.mcknight@sentry.io>
2023-10-17 14:13:30 -0800

    fix data race in SentryNetworkTracker.isCaptureFailedRequestsEnabled

--- scripts/no-changes-in-high-risk-files.sh
+++ scripts/no-changes-in-high-risk-files.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 ACTUAL=$(shasum -a 256 ./Sources/Sentry/SentryNSURLSessionTaskSearch.m ./Sources/Sentry/SentryNetworkTracker.m ./Sources/Sentry/SentryUIViewControllerSwizzling.m ./Sources/Sentry/SentryNSDataSwizzling.m ./Sources/Sentry/SentrySubClassFinder.m ./Sources/Sentry/SentryCoreDataSwizzling.m ./Sources/Sentry/SentrySwizzleWrapper.m ./Sources/Sentry/include/SentrySwizzle.h ./Sources/Sentry/SentrySwizzle.m)
 EXPECTED="819d5ca5e3db2ac23c859b14c149b7f0754d3ae88bea1dba92c18f49a81da0e1  ./Sources/Sentry/SentryNSURLSessionTaskSearch.m
-57056426d77fd1cc27e72bdcea4bbf617dd8c1e82eecf003df8f0e348aae90ab  ./Sources/Sentry/SentryNetworkTracker.m
+526c44c5f06a1d03fcb7ed813cee8aac520ffe539b911a44d5d8bd1c85e91a0e  ./Sources/Sentry/SentryNetworkTracker.m
 128dee523c75c121e9b8b767d84a2a137e5b60498c9f3ba604aad3c935d780d0  ./Sources/Sentry/SentryUIViewControllerSwizzling.m
 e95e62ec7363984f20c78643bb7d992a41a740f97e1befb71525ac34caf88b37  ./Sources/Sentry/SentryNSDataSwizzling.m
 9ad05dd8dd29788cba994736fdcd3bbde59a94e32612640d11f4f9c38ad6610e  ./Sources/Sentry/SentrySubClassFinder.m
